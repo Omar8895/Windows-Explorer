067d49cda30f14ac5e1df87e1dfa92b73ee88e4f
Omar8895 <120728769+Omar8895@users.noreply.github.com>
2023-02-11 09:53:19 +0200

    Add files via upload

---
+++ Windows Explorer/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace lab11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(94, 76);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(252, 199);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.Click += new System.EventHandler(this.listBox1_Click);
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(493, 76);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(252, 199);
+            this.listBox2.TabIndex = 1;
+            this.listBox2.Click += new System.EventHandler(this.listBox2_Click);
+            this.listBox2.DoubleClick += new System.EventHandler(this.listBox2_DoubleClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(139, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(159, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(546, 33);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(159, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(211, 304);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 30);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Copy";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(371, 304);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 30);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Delete ";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Yellow;
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button3.Location = new System.Drawing.Point(534, 304);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 30);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Back";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Red;
+            this.button4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button4.Location = new System.Drawing.Point(371, 107);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 37);
+            this.button4.TabIndex = 7;
+            this.button4.Text = ">>";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.button5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button5.Location = new System.Drawing.Point(371, 181);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 35);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "<<";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Windows Explorer/Form1.cs
@@ -0,0 +1,573 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace lab11
+{
+    public partial class Form1 : Form
+    {
+
+        //Drives appearance
+
+        DriveInfo[] my_Drives = DriveInfo.GetDrives();
+
+        DirectoryInfo Directory_Item;
+        string selected;
+        string selected1;
+        bool flag1 = false;
+        bool flag2 = false;
+        string last_change;
+        public Form1()
+        {
+            InitializeComponent();
+            listBox1.Items.AddRange(my_Drives);
+            listBox2.Items.AddRange(my_Drives);
+        }
+
+        //selection for listbox 1
+        private void listBox1_Click(object sender, EventArgs e)
+        {
+            selected= listBox1.SelectedItem.ToString();
+            flag1 = true;
+            flag2 = false;
+        }
+
+
+        //open selection components
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            try
+            {
+
+                if (selected == ".")
+                {
+                    listBox1.Items.Clear();
+
+                    if (textBox1.Text.Length < 4)
+                    {
+                        listBox1.Items.AddRange(my_Drives);
+                    }
+
+                    else
+                    {
+                        string parent = Directory.GetParent(textBox1.Text).ToString();
+
+                        foreach (string entry in Directory.GetDirectories(parent))
+                        {
+                            listBox1.Items.Add(entry);
+
+                        }
+
+                        foreach (string entry in Directory.GetFiles(parent))
+                        {
+                            listBox1.Items.Add(entry);
+                        }
+
+                        listBox1.Items.Add(".");
+                        listBox1.Items.Add("..");
+
+                        flag1 = true;
+                        flag2 = false;
+
+                        textBox1.Text = parent;
+                    }
+                }
+
+                else if (selected == "..") 
+                {
+                    listBox1.Items.Clear();
+                    listBox1.Items.AddRange(my_Drives);
+
+                }
+                else
+
+                {
+
+                    listBox1.Items.Clear();
+
+
+                    foreach (string entry in Directory.GetDirectories(selected))
+                    {
+                        listBox1.Items.Add(entry);
+
+                    }
+
+                    foreach (string entry in Directory.GetFiles(selected))
+                    {
+                        listBox1.Items.Add(entry);
+                    }
+                    listBox1.Items.Add(".");
+                    listBox1.Items.Add("..");
+                    last_change = textBox1.Text;
+                    flag1 = true;
+                    flag2 = false;
+                    textBox1.Text = selected;
+
+                }
+
+                
+            }
+
+            catch
+            {
+
+         
+                MessageBox.Show("Can't open file");
+
+                foreach (string entry in Directory.GetDirectories(textBox1.Text))
+                {
+                    listBox1.Items.Add(entry);
+
+                }
+
+                foreach (string entry in Directory.GetFiles(textBox1.Text))
+                {
+                    listBox1.Items.Add(entry);
+                }
+
+                listBox1.Items.Add(".");
+                listBox1.Items.Add("..");
+            }
+  
+        }
+
+        //selection for listbox 2
+        private void listBox2_Click(object sender, EventArgs e)
+        {
+            selected1 = listBox2.SelectedItem.ToString();
+            flag1 = false;
+            flag2 = true;
+        }
+
+        //open selection component
+        private void listBox2_DoubleClick(object sender, EventArgs e)
+        {
+            try
+            {
+                if ( selected1 == ".")
+                {
+                    listBox2.Items.Clear();
+
+                    if (textBox2.Text.Length < 4)
+                    {
+                        listBox2.Items.AddRange(my_Drives);
+                    }
+
+                    else
+                    {
+                        string parent2 = Directory.GetParent(textBox2.Text).ToString();
+
+                        foreach (string entry in Directory.GetDirectories(parent2))
+                        {
+                            listBox2.Items.Add(entry);
+
+                        }
+
+                        foreach (string entry in Directory.GetFiles(parent2))
+                        {
+                            listBox2.Items.Add(entry);
+                        }
+
+                        listBox2.Items.Add(".");
+                        listBox2.Items.Add("..");
+
+                        flag1 = false;
+                        flag2 = true;
+
+                        textBox2.Text = parent2;
+                    }
+                }
+
+                else if (selected1 == "..") 
+                {
+
+                    listBox2.Items.Clear();
+                    listBox2.Items.AddRange(my_Drives);
+                }
+
+                else
+                {
+
+                    listBox2.Items.Clear();
+                    foreach (string entry in Directory.GetDirectories(selected1))
+                    {
+                        listBox2.Items.Add(entry);
+
+                    }
+
+                    foreach (string entry in Directory.GetFiles(selected1))
+                    {
+                        listBox2.Items.Add(entry);
+                    }
+                    listBox2.Items.Add(".");
+                    listBox2.Items.Add("..");
+                    last_change = textBox2.Text;
+                    flag1 = false;
+                    flag2 = true;
+
+                    textBox2.Text = selected1;
+                }
+            }
+                
+            catch
+            {
+
+
+                MessageBox.Show("Can't open file");
+
+
+                foreach (string entry in Directory.GetDirectories(textBox2.Text))
+                {
+                    listBox2.Items.Add(entry);
+
+                }
+
+                foreach (string entry in Directory.GetFiles(textBox2.Text))
+                {
+                    listBox2.Items.Add(entry);
+                }
+
+                listBox2.Items.Add(".");
+                listBox2.Items.Add("..");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (flag1 == true)
+            {
+                listBox1.Items.Clear();
+
+                if (textBox1.Text.Length < 4)
+                {
+                    listBox1.Items.AddRange(my_Drives);
+                }
+
+                else
+                {
+                    string parent = Directory.GetParent(textBox1.Text).ToString();
+
+                    foreach (string entry in Directory.GetDirectories(parent))
+                    {
+                        listBox1.Items.Add(entry);
+
+                    }
+
+                    foreach (string entry in Directory.GetFiles(parent))
+                    {
+                        listBox1.Items.Add(entry);
+                    }
+
+                    listBox1.Items.Add(".");
+                    listBox1.Items.Add("..");
+
+                    flag1 = true;
+                    flag2 = false;
+
+                    textBox1.Text = parent;
+                }
+            }
+
+                if (flag2 == true) 
+                {
+                    listBox2.Items.Clear();
+
+                    if (textBox2.Text.Length < 4)
+                    {
+                        listBox2.Items.AddRange(my_Drives);
+                        textBox2.Text = "";
+                    }
+
+                    else
+                    {
+                        string parent2 = Directory.GetParent(textBox2.Text).ToString();
+
+                        foreach (string entry in Directory.GetDirectories(parent2))
+                        {
+                            listBox2.Items.Add(entry);
+
+                        }
+
+                        foreach (string entry in Directory.GetFiles(parent2))
+                        {
+                            listBox2.Items.Add(entry);
+                        }
+
+                    listBox2.Items.Add(".");
+                    listBox2.Items.Add("..");
+                    flag1 = false;
+                        flag2 = true;
+
+                        textBox2.Text = parent2;
+                    
+                }
+                
+            }
+        }
+
+        //Moving from listbox 1 to listbox 2
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Directory_Item = new DirectoryInfo(selected);
+            File.Move(selected, selected1 + "/" + Directory_Item.Name);
+            listBox1.Items.Clear();
+
+            foreach (string entry in Directory.GetDirectories(textBox1.Text))
+            {
+                listBox1.Items.Add(entry);
+
+            }
+
+            foreach (string entry in Directory.GetFiles(textBox1.Text))
+            {
+                listBox1.Items.Add(entry);
+            }
+
+            listBox1.Items.Add(".");
+            listBox1.Items.Add("..");
+        }
+
+
+        //moving from listbox 2 to listbox 1
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Directory_Item = new DirectoryInfo(selected);
+            File.Move(selected1, selected + "/" + Directory_Item.Name);
+            listBox2.Items.Clear();
+
+            foreach (string entry in Directory.GetDirectories(textBox2.Text))
+            {
+                listBox2.Items.Add(entry);
+
+            }
+
+            foreach (string entry in Directory.GetFiles(textBox2.Text))
+            {
+                listBox2.Items.Add(entry);
+            }
+
+            listBox2.Items.Add(".");
+            listBox2.Items.Add("..");
+        }
+
+        //Copying files
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+                if (listBox1.SelectedItem != null && listBox2.SelectedItem != null)
+                {
+                    if ( flag1 == true )
+                    {
+                         string destinationFile = Path.Combine(selected1, Path.GetFileName(selected));
+                        
+                        try
+                        {
+                            File.Copy(selected, destinationFile, true);
+
+                            listBox2.Items.Clear();
+
+
+                        foreach (string entry in Directory.GetDirectories(textBox2.Text))
+                        {
+                            listBox2.Items.Add(entry);
+
+                        }
+
+                        foreach (string entry in Directory.GetFiles(textBox2.Text))
+                        {
+                            listBox2.Items.Add(entry);
+                        }
+
+                        listBox2.Items.Add(".");
+                        listBox2.Items.Add("..");
+
+                        MessageBox.Show("File copied successfully");
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Error copying file: " + ex.Message);
+                        }
+                    }
+     
+
+                    if (flag2 == true)
+                    {
+                        string destinationFile = Path.Combine(selected, Path.GetFileName(selected1));
+
+                        try
+                        {
+                            File.Copy(selected1, destinationFile, true);
+                            listBox2.Items.Clear();
+
+
+                        foreach (string entry in Directory.GetDirectories(textBox1.Text))
+                        {
+                            listBox1.Items.Add(entry);
+
+                        }
+
+                        foreach (string entry in Directory.GetFiles(textBox1.Text))
+                        {
+                            listBox1.Items.Add(entry);
+                        }
+
+                        listBox2.Items.Add(".");
+                        listBox2.Items.Add("..");
+                        MessageBox.Show("File copied successfully");
+                        }
+                        catch (Exception ex)
+                        {
+                             MessageBox.Show("Error copying file: " + ex.Message);
+                        }
+                    }
+    
+                }
+
+        }
+
+
+        //Deleting files
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if ( flag1  == true)
+            {
+                if (File.Exists(selected))
+                {
+                    try
+                    {
+                        File.Delete(selected);
+
+                        listBox1.Items.Clear();
+
+
+                        foreach (string entry in Directory.GetDirectories(textBox1.Text))
+                        {
+                            listBox1.Items.Add(entry);
+
+                        }
+
+                        foreach (string entry in Directory.GetFiles(textBox1.Text))
+                        {
+                            listBox1.Items.Add(entry);
+                        }
+
+                        listBox1.Items.Add(".");
+                        listBox1.Items.Add("..");
+                    }
+
+                    catch (IOException o)
+                    {
+                        MessageBox.Show(o.Message);
+                    }
+
+
+                }
+                else if (Directory.Exists(selected))
+
+                {
+                    try
+                    {
+                        Directory.Delete(selected);
+
+                        listBox1.Items.Clear();
+
+
+                        foreach (string entry in Directory.GetDirectories(textBox1.Text))
+                        {
+                            listBox1.Items.Add(entry);
+
+                        }
+
+                        foreach (string entry in Directory.GetFiles(textBox1.Text))
+                        {
+                            listBox1.Items.Add(entry);
+                        }
+
+                        listBox1.Items.Add(".");
+                        listBox1.Items.Add("..");
+                    }
+
+                    catch (IOException o)
+                    {
+                        MessageBox.Show(o.Message);
+                    }
+
+                } 
+
+            }
+
+            if (flag2 == true)
+            {
+              if (File.Exists(selected1))
+                {
+                    try
+                    {
+                        File.Delete(selected1);
+
+                        listBox2.Items.Clear();
+
+
+                        foreach (string entry in Directory.GetDirectories(textBox2.Text))
+                        {
+                            listBox2.Items.Add(entry);
+
+                        }
+
+                        foreach (string entry in Directory.GetFiles(textBox2.Text))
+                        {
+                            listBox2.Items.Add(entry);
+                        }
+
+                        listBox2.Items.Add(".");
+                        listBox2.Items.Add("..");
+                    }
+
+                    catch (IOException o)
+                    {
+                        MessageBox.Show(o.Message);
+                    }
+                 }
+
+                  else if (Directory.Exists(selected1))
+
+                     {
+                        try
+                        {
+                            Directory.Delete(selected1);
+
+                            listBox2.Items.Clear();
+
+
+                            foreach (string entry in Directory.GetDirectories(textBox2.Text))
+                            {
+                                listBox2.Items.Add(entry);
+
+                            }
+
+                            foreach (string entry in Directory.GetFiles(textBox2.Text))
+                            {
+                                listBox2.Items.Add(entry);
+                            }
+
+                        listBox2.Items.Add(".");
+                        listBox2.Items.Add("..");
+                    }
+
+                        catch (IOException o)
+                        {
+                            MessageBox.Show(o.Message);
+                        }
+
+                }
+            }
+        }
+    }
+ }
+
